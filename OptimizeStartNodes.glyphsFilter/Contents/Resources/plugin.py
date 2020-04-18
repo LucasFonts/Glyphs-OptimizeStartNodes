d82ea69b2908964c8289af70e5fd513391c6767a
Rainer Scheichelbauer <res@glyphsapp.com>
2020-04-18 15:41:14 +0200

    Glyphs 2/3, Python 2/3 compatibility, localisation

--- OptimizeStartNodes.glyphsFilter/Contents/Resources/plugin.py
+++ OptimizeStartNodes.glyphsFilter/Contents/Resources/plugin.py
@@ -1,30 +1,25 @@
 # encoding: utf-8
-
 from __future__ import division, print_function, unicode_literals
-import objc
-from GlyphsApp import Glyphs
-from GlyphsApp.plugins import FilterWithoutDialog
 
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
 
 class OptimizeStartNodes(FilterWithoutDialog):
 
     @objc.python_method
     def settings(self):
-        self.menuName = Glyphs.localize(
-            {
-                'en': u'Optimize Start Nodes',
-                'de': u'Startpunkte optimieren'
-            }
-        )
-        self.keyboardShortcut = None  # With Cmd+Shift
+        self.menuName = Glyphs.localize({
+            'en': 'Optimize Start Nodes',
+            'de': 'Startpunkte optimieren',
+            'es': 'Optimizar los primeros nodos',
+            'fr': 'Optimiser les nœuds prémiers',
+        })
 
     @objc.python_method
     def filter(self, layer, inEditView, customParameters):
         for p in layer.paths:
-            node_types = [
-                n.type
-                for n in p.nodes
-            ]
+            node_types = [n.type for n in p.nodes]
             if node_types[-1] != "line":
                 if "line" in node_types:
                     i = node_types.index("line")
