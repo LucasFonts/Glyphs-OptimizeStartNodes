beb860e491a6fe0926ca29678e3448058d52fcf2
Jens Kutilek <webmail@kutilek.de>
2020-04-20 10:19:22 +0200

    Cleanup after merge

--- OptimizeStartNodes.glyphsFilter/Contents/Resources/plugin.py
+++ OptimizeStartNodes.glyphsFilter/Contents/Resources/plugin.py
@@ -2,8 +2,9 @@
 from __future__ import division, print_function, unicode_literals
 
 import objc
-from GlyphsApp import *
-from GlyphsApp.plugins import *
+from GlyphsApp import Glyphs
+from GlyphsApp.plugins import FilterWithoutDialog
+
 
 class OptimizeStartNodes(FilterWithoutDialog):
 
@@ -19,7 +20,11 @@ class OptimizeStartNodes(FilterWithoutDialog):
     @objc.python_method
     def filter(self, layer, inEditView, customParameters):
         for p in layer.paths:
-            node_types = [n.type for n in p.nodes]
+            node_types = [
+                n.type
+                for n in
+                p.nodes
+            ]
             if node_types[-1] != "line":
                 if "line" in node_types:
                     i = node_types.index("line")
